a0b14ccc60a021d8a3f1f987f479640aad00d4f7
jovin-nss <jovin@nss>
2020-10-22 01:27:18 +0530

    error fixes

--- inspec-redis/controls/control_01.rb
+++ inspec-redis/controls/control_01.rb
@@ -1,12 +1,11 @@
 control 'redis-01' do 
     impact 7.0
     title 'Check redis.conf file ownership & permissions'
-    desc 'The redis configuration file should be owned by root, root should have write access while others should have read-only access. '
+    desc 'The redis configuration file should be owned by redis_admin, and only redis_admin should have write access while others should have read-only access. '
 
     describe file('/usr/local/etc/redis/redis.conf') do 
         its('owner') { should eq "redis_admin" }
         its('group') { should eq "redis_admin" }
         its('mode') { should cmp '0600' }
     end
-
 end
--- inspec-redis/controls/control_04.rb
+++ inspec-redis/controls/control_04.rb
@@ -1,7 +1,7 @@
 control 'redis-04' do
     impact 8.2
     title 'Check if Redis authentication is enabled and if so, check for the password complexity '
-    desc 'Redis server must be configured for authenticated access. The defined password must contain at least one letter, at least one number, and be longer than 18 charaters.'
+    desc 'Redis server must be configured for authenticated access. The defined password must contain at least one letter, at least one number, and be longer than 18 characters.'
     describe file('/usr/local/etc/redis/redis.conf') do
         its('content') { should match(%r{^requirepass}) }
     end
